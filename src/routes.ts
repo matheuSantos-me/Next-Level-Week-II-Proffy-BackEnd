71d1ab6802bb836c35badad81af28cfc51d5c554
Matheus Santos <matheus.santos@server.signainfo.com.br>
2020-08-04 23:32:03 -0300

    docs: update routes

--- src/routes.ts
+++ src/routes.ts
@@ -23,35 +23,46 @@ routes.post('/classes', async (req, res) => {
 
   const trx = await db.transaction()
 
-  const insertedUsersIds = await trx('users').insert({
-    name,
-    avatar,
-    whatsapp,
-    bio
-  })
+  try {
+    const insertedUsersIds = await trx('users').insert({
+      name,
+      avatar,
+      whatsapp,
+      bio
+    })
+  
+    const user_id = insertedUsersIds[0]
+  
+    const insertedClassesIds = await trx('classes').insert({
+      subject,
+      cost,
+      user_id
+    })
+  
+    const class_id = insertedClassesIds[0]
+  
+    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+      return {
+        class_id,
+        week_day: scheduleItem.week_day,
+        from: convertHourToMinute(scheduleItem.from),
+        to: convertHourToMinute(scheduleItem.to)
+      }
+    })
+    
+    await trx('class_schedule').insert(classSchedule)
+    
+    await trx.commit()   
+    return res.status(201).send()
 
-  const user_id = insertedUsersIds[0]
+  } catch (e) {
+    await trx.rollback()
 
-  const insertedClassesIds = await trx('classes').insert({
-    subject,
-    cost,
-    user_id
-  })
+    return res.status(400).json({
+      error: 'Unexpected orror while creating new class'
+    })
+  }
 
-  const class_id = insertedClassesIds[0]
-
-  const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-    return {
-      class_id,
-      week_day: scheduleItem.week_day,
-      from: convertHourToMinute(scheduleItem.from),
-      to: convertHourToMinute(scheduleItem.to)
-    }
-  })
-
-  await trx('class_schedule').insert(classSchedule)
-
-  return res.send()
 })
 
 export default routes
